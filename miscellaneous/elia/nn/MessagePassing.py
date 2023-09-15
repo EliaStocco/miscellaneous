fixing normalization

miscellaneous/elia/nn/MessagePassing.py
@@ -112,7 +112,7 @@ class MessagePassing(torch.nn.Module):
             -1: torch.tanh,
         }
 
-        self.layers = torch.nn.ModuleList()
+        tmp = torch.nn.ModuleList()
 
         for _ in range(layers):
             irreps_scalars = o3.Irreps(
@@ -147,13 +147,18 @@ class MessagePassing(torch.nn.Module):
                 irreps_node, self.irreps_node_attr, self.irreps_edge_attr, gate.irreps_in, fc_neurons, num_neighbors
             )
             irreps_node = gate.irreps_out
-            self.layers.append(Compose(conv, gate))
+            tmp.append(Compose(conv, gate))
 
-        self.layers.append(
+        tmp.append(
             Convolution(
                 irreps_node, self.irreps_node_attr, self.irreps_edge_attr, self.irreps_node_output, fc_neurons, num_neighbors
             )
         )
+
+        #self.layers = torch.nn.ModuleList(tmp)
+        self.layers = tmp
+
+        pass
 
     def forward(self:T, node_features, node_attr, edge_src, edge_dst, edge_attr, edge_scalars) -> torch.Tensor:
         if self.debug:

miscellaneous/elia/nn/SabiaNetwork.py
@@ -18,16 +18,16 @@ T = TypeVar('T', bound='SabiaNetwork')
 
 class SabiaNetwork(torch.nn.Module):
 
-    default_dtype : torch.dtype
-    lmax : int
-    max_radius: float
-    number_of_basis : int
-    num_nodes : int
-    pool_nodes : bool
-    debug : bool
-    mp : MessagePassing
-    irreps_in : o3.Irreps
-    irreps_out : o3.Irreps
+    default_dtype = torch.float64 #: torch.dtype
+    # lmax : int
+    # max_radius: float
+    # number_of_basis : int
+    # num_nodes : int
+    # pool_nodes : bool
+    # debug : bool
+    # mp : MessagePassing
+    # irreps_in : o3.Irreps
+    # irreps_out : o3.Irreps
 
     def __init__(self,
         irreps_in,
@@ -43,12 +43,12 @@ class SabiaNetwork(torch.nn.Module):
         p=["o","e"],
         debug=False,
         pool_nodes=True,
-        default_dtype=torch.float64,
+        # default_dtype=torch.float64,
         **argv) -> None:
         
         super().__init__(**argv)
 
-        self.default_dtype = default_dtype
+        # self.default_dtype = default_dtype
         torch.set_default_dtype(self.default_dtype)
         self.lmax = lmax
         self.max_radius = max_radius
@@ -81,10 +81,12 @@ class SabiaNetwork(torch.nn.Module):
             num_neighbors=num_neighbors,
         )
 
-        self.sh = o3.SphericalHarmonics( range(self.lmax + 1), True, normalization="component")
+        self._sh = o3.SphericalHarmonics( range(self.lmax + 1), True, normalization="component")
 
         self.irreps_in = self.mp.irreps_node_input
         self.irreps_out = self.mp.irreps_node_output
+
+        pass
     
     # Overwriting preprocess method of SimpleNetwork to adapt for periodic boundary data
     def preprocess(self, data: Union[Data, Dict[str, torch.Tensor]]) -> torch.Tensor:
@@ -129,7 +131,7 @@ class SabiaNetwork(torch.nn.Module):
         # del data
 
         # edge_attr = o3.spherical_harmonics( range(self.lmax + 1), edge_vec, True, normalization="component")
-        edge_attr = self.sh(edge_vec)
+        edge_attr = self._sh(edge_vec)
         
         # Edge length embedding
         edge_length = edge_vec.norm(dim=1)

miscellaneous/elia/nn/SabiaNetworkManager.py
@@ -2,7 +2,7 @@ from torch_geometric.data import Data
 import torch
 from torch.func import jacrev
 import numpy as np
-from .SabiaNetwork import SabiaNetwork
+
 from .iPIinterface import iPIinterface
 from .Methods4Training import EDFMethods4Training
 from .Methods4AngularOutput import Methods4AngularOutput
@@ -16,18 +16,7 @@ T = TypeVar('T', bound='SabiaNetworkManager')
 __all__ = ["SabiaNetworkManager"]
 
 # @froze
-class SabiaNetworkManager(EDFMethods4Training,iPIinterface,SabiaNetwork):
-
-    # __slots__ = ("output","reference","_forces","_bec","_X","ref_dipole","ref_pos")
-
-    # output:str
-    # lattice: torch.Tensor
-    # max_radius: float
-    # _radial_cutoff: float
-    # symbols: list
-    # x: Data
-    # R:torch.Tensor
-    # _make_datapoint:callable
+class SabiaNetworkManager(EDFMethods4Training,iPIinterface):
 
     def __init__(self: T, 
                  output: str = "ED",
@@ -71,9 +60,9 @@ class SabiaNetworkManager(EDFMethods4Training,iPIinterface,SabiaNetwork):
 
         if self.reference :
 
-            if dipole is None :
-                raise ValueError("'dipole' can not be 'None'")
-            self.ref_dipole = torch.tensor(dipole)
+            # if dipole is None :
+            #     raise ValueError("'dipole' can not be 'None'")
+            # self.ref_dipole = torch.tensor(dipole)
             
             if pos is None :
                 raise ValueError("'pos' can not be 'None'")
@@ -299,162 +288,57 @@ class SabiaNetworkManager(EDFMethods4Training,iPIinterface,SabiaNetwork):
 
         return y
 
-    # # @staticmethod
-    # def get_pred(self: T, X: Data) -> torch.tensor:
-    #     """return Energy, Polarization and Forces"""
+    # def check_equivariance(self:T,X: Data,angles=None)->np.ndarray:
 
-    #     N = {"E": 1, "D": 3, "ED": 4, "EF": 1+3*X.Natoms[0], "EDF": 1+3+3*X.Natoms[0]}
-    #     N = N[self.output]
-    #     batch_size = len(np.unique(X.batch))
-    #     y = torch.zeros((batch_size, N))
+    #     from e3nn import o3
 
-    #     if self.output in ["E", "ED","D"]:
-    #         y = self(X)
+    #     with torch.no_grad():
 
-    #     elif self.output == "EDF":
-    #         EP = self(X)
-    #         y[:, 0] = EP[:, 0]         # 1st column for the energy
-    #         y[:, 1:4] = EP[:, 1:4]     # 2nd to 4th columns for the dipole
-    #         y[:, 4:] = self.forces(X) # other columns for the forces
+    #         batch_size = len(np.unique(X.batch))
+    #         y = np.zeros(batch_size)
 
-    #     elif self.output == "EF":
-    #         EP = self(X)
-    #         y[:, 0]  = EP[:, 0]         # 1st column for the energy
-    #         y[:, 1:] = self.forces(X)  # other columns for the forces
+    #         irreps_out = o3.Irreps(self.irreps_out)
 
+    #         if angles is None :
+    #             angles = o3.rand_angles()
+
+    #         for n in range(batch_size):
+
+    #             irreps_in = "{:d}x1o".format(int(X.Natoms[n]))
+    #             irreps_in = o3.Irreps(irreps_in)
+
+    #             # prepare data -> prepare self.R and self._X
+
+    #             # rotate output
+    #             x,R = self._prepare(X,n)
+    #             R_out = irreps_out.D_from_angles(*angles)
+    #             y0 = self(x)
+    #             rot_out = torch.einsum("ij,zj->zi",R_out,y0)
+
+    #             # rotate input
+    #             R_in  = irreps_in.D_from_angles(*angles)
+    #             x,R = self._prepare(X,n,R_in)
+    #             #R = R.reshape((1,-1))
+
+                
+                
+    #             # R = x.pos.reshape((1,-1))                
+    #             # rot_in  = torch.einsum("ij,zj->zi",R_in,R)
+
+    #             # l = x.lattice.reshape((1,-1))
+    #             # l_in  = torch.einsum("ij,zj->zi",R_in,l)
+                
+    #             # x.pos     = rot_in.reshape((-1,3))
+    #             # x.lattice = l_in.reshape((-1,3))
+
+    #             #x,R_ = self._prepare(X,n,replace_pos=rot_in.reshape((-1,3)))
+    #             out_rot = self(x)
+
+    #             thr = torch.norm(rot_out - out_rot)
+                
+    #             y[n] = thr
+        
     #     return y
-
-    # # @staticmethod
-    # def get_real(self:T,X: Data) -> torch.tensor:
-    #     """return Energy, Polarization and/or Forces"""
-
-    #     # 'EPF' has to be modified in case we have different molecules in the dataset
-    #     N = {"E": 1, "EF": 1+3*X.Natoms[0], "D": 3, "ED": 4, "EDF": 1+3+3*X.Natoms[0]}
-    #     N = N[self.output]
-    #     batch_size = len(np.unique(X.batch))
-
-    #     # if batch_size > 1 :
-
-    #     y = torch.zeros((batch_size, N))
-
-    #     if self.output in ["E", "EF", "ED", "EDF"]:
-    #         y[:, 0] = X.energy
-
-    #         if self.output in ["ED", "EDF"]:
-    #             y[:, 1:4] = X.dipole.reshape((batch_size, -1))
-
-    #         elif self.output == "EDF":
-    #             y[:, 4:] = X.forces.reshape((batch_size, -1))
-
-    #         if self.output == "EF":
-    #             y[:, 1:] = X.forces.reshape((batch_size, -1))
-
-    #     elif self.output == "D":
-    #         y[:,0:3] = X.dipole.reshape((batch_size, -1))
-
-    #     return y
-
-    # def loss(self:T,lE:float=None,lF:float=None,lP:float=None)->callable:
-
-    #     lE = lE if lE is not None else 1.0
-    #     lF = lF if lF is not None else 1.0
-    #     lP = lP if lP is not None else 1.0
-
-    #     if self.output in ["E","D"]:
-    #         return MSELoss() #MSELoss(reduction='mean') # MSELoss(reduce='sum')
-    #         #return lambda x,y: MSELoss()(x,y)
-        
-    #     elif self.output == "ED":
-    #         def loss_EP(x,y):
-    #             E = MSELoss()(x[:,0],y[:,0])
-    #             P = MSELoss()(x[:,1:4],y[:,1:4])
-    #             return lE * E + lP * P
-    #         return loss_EP
-        
-    #     elif self.output == "EF":
-    #         def loss_EF(x,y):
-    #             E = MSELoss()(x[:,0],y[:,0])
-    #             F = MSELoss()(x[:,1:],y[:,1:])
-    #             return lE * E + lF * F
-    #         return loss_EF
-        
-    #     elif self.output == "EDF":
-    #         def loss_EPF(x,y):
-    #             E = MSELoss()(x[:,0],y[:,0])
-    #             P = MSELoss()(x[:,1:4],y[:,1:4])
-    #             F = MSELoss()(x[:,4:],y[:,4:])
-    #             return lE * E + lP * P + lF * F
-    #         return loss_EPF
-        
-    #     else :
-    #         raise ValueError("error in output mode")
-        
-    # @staticmethod
-    # def correlation(x:torch.tensor,y:torch.tensor):
-
-    #     #N = {"E": 1, "P": 3, "EP": 4, "EPF": 1+3+3*X.Natoms[0]}
-
-    #     x = x.detach().numpy()
-    #     y = y.detach().numpy()
-
-    #     N = x.shape[1]
-    #     out = np.zeros(N)
-    #     for i in range(N):
-    #         out[i] = spearmanr(x[:,i],y[:,i]).correlation
-
-    #     return out
-
-    def check_equivariance(self:T,X: Data,angles=None)->np.ndarray:
-
-        from e3nn import o3
-
-        with torch.no_grad():
-
-            batch_size = len(np.unique(X.batch))
-            y = np.zeros(batch_size)
-
-            irreps_out = o3.Irreps(self.irreps_out)
-
-            if angles is None :
-                angles = o3.rand_angles()
-
-            for n in range(batch_size):
-
-                irreps_in = "{:d}x1o".format(int(X.Natoms[n]))
-                irreps_in = o3.Irreps(irreps_in)
-
-                # prepare data -> prepare self.R and self._X
-
-                # rotate output
-                x,R = self._prepare(X,n)
-                R_out = irreps_out.D_from_angles(*angles)
-                y0 = self(x)
-                rot_out = torch.einsum("ij,zj->zi",R_out,y0)
-
-                # rotate input
-                R_in  = irreps_in.D_from_angles(*angles)
-                x,R = self._prepare(X,n,R_in)
-                #R = R.reshape((1,-1))
-
-                
-                
-                # R = x.pos.reshape((1,-1))                
-                # rot_in  = torch.einsum("ij,zj->zi",R_in,R)
-
-                # l = x.lattice.reshape((1,-1))
-                # l_in  = torch.einsum("ij,zj->zi",R_in,l)
-                
-                # x.pos     = rot_in.reshape((-1,3))
-                # x.lattice = l_in.reshape((-1,3))
-
-                #x,R_ = self._prepare(X,n,replace_pos=rot_in.reshape((-1,3)))
-                out_rot = self(x)
-
-                thr = torch.norm(rot_out - out_rot)
-                
-                y[n] = thr
-        
-        return y
 
         
 def main():

miscellaneous/elia/nn/hyper_train.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
-from copy import copy
+from copy import copy, deepcopy
 import torch
 import os
 from miscellaneous.elia.nn import train
 from ..functions import add_default
+from itertools import product
 
 def hyper_train_at_fixed_model( net:torch.nn.Module,\
                                 all_bs:list,\
@@ -12,6 +13,7 @@ def hyper_train_at_fixed_model( net:torch.nn.Module,\
                                 epochs,\
                                 loss:callable,\
                                 datasets:dict,\
+                                parameters:dict,\
                                 opts:dict=None):
     
     ##########################################
@@ -19,8 +21,11 @@ def hyper_train_at_fixed_model( net:torch.nn.Module,\
     default = { #"dataloader":{"shuffle":True},\
                 #"disable":False,\
                 #"Natoms":1,
-                "name":"untitled",
-                "output_folder":"results"}
+                "max_try" : 5,
+                #"trial" : None,
+                # "name":"untitled",
+                #"output_folder":"results"
+            }
     opts = add_default(opts,default)
 
     ##########################################
@@ -43,70 +48,115 @@ def hyper_train_at_fixed_model( net:torch.nn.Module,\
     del datasets
     # test_dataset  = datasets["test"]
 
-    Ntot = len(all_bs)*len(all_lr)
-    print("\n")
-    print("all batch_size:",all_bs)
-    print("all lr:",all_lr)
-    print("\n")
-    df = pd.DataFrame(columns=["bs","lr","file"],index=np.arange(len(all_bs)*len(all_lr)))
+    
 
-    init_model = copy(net) 
+    ##########################################
+    # preparing cycle over hyper-parameters
+    if parameters["grid"] :
+        hyper_pars = product(all_bs, all_lr)
+        Ntot = len(all_bs)*len(all_lr)
+    else :
+        if len(all_bs) != len(all_lr) :
+            raise ValueError("batch sizes and learning rates should have the same lenght when 'grid'=True")
+        hyper_pars = zip(all_bs, all_lr)
+        Ntot = len(all_bs)
 
-    n = 0
-    info = "all good"
-    max_try = 5
-    for batch_size in all_bs :
+    #Ntot = len(hyper_pars)
+    df = pd.DataFrame(columns=["bs","lr","file"],index=np.arange(Ntot))
 
-        for lr in all_lr:
+    if parameters["trial"] is not None :
+        if parameters["trial"] in [0,1]:
+            parameters["trial"] = None
+        else :
+            df["trial"] = None
 
-            df.at[n,"bs"] = batch_size
-            df.at[n,"lr"] = lr
-            df.at[n,"file"] = "{:s}.bs={:d}.lr={:.1e}".format(opts["name"],batch_size,lr)
-            
-            print("#########################\n")
-            print("batch_size={:d}\t|\tlr={:.1e}\t|\tn={:d}/{:d}".format(batch_size,lr,n+1,Ntot))
+    if parameters["task_time"] == -1 and parameters["max_time"] != 1 :
+        parameters["task_time"] = ( parameters["max_time"] - 60 ) / Ntot
+  
 
-            #print("\n\trebuilding network...\n")
-            net = copy(init_model)
-            
-            hyperparameters = {
-                'batch_size': batch_size,
-                'n_epochs'  : epochs.at[batch_size,lr],
-                'optimizer' : "Adam",
-                'lr'        : lr,
-                'loss'      : loss 
-            }
+    ##########################################
+    # training function
+    def run(n,bs,lr):
 
-            #print("\n\ttraining network...\n")
-            count_try = 0
-            while (info == "try again" and count_try < max_try) or count_try == 0 :
+        info = "all good"
 
-                if info == "try again":
-                    print("\nLet's try again\n")
+        df.at[n,"bs"] = bs
+        df.at[n,"lr"] = lr
 
-                model, arrays, corr, info = \
-                    train(  model=net,
-                            train_dataset=train_dataset,
-                            val_dataset=val_dataset,
-                            hyperparameters=hyperparameters,
-                            get_pred=net.get_pred,
-                            get_real=net.get_real, #lambda X: net.get_real(X=X,output=net.output),
-                            output=opts["output_folder"],
-                            # correlation = net.correlation,
-                            name=df.at[n,"file"],
-                            opts=opts)
-                count_try += 1
+        if parameters["trial"] is not None :
+            df[n,"trial"] = n_trial
+            df.at[n,"file"] = "{:s}.bs={:d}.lr={:.1e}.trial={:d}".format(parameters["name"],bs,lr,n_trial)
+        else :
+            df.at[n,"file"] = "{:s}.bs={:d}.lr={:.1e}".format(parameters["name"],bs,lr)
+        
+        print("#########################\n")
+        print("bs={:d}\t|\tlr={:.1e}\t|\tn={:d}/{:d}".format(bs,lr,n+1,Ntot))
+
+        #print("\n\trebuilding network...\n")
+        net = deepcopy(init_model)
+        
+        hyperparameters = {
+            'bs': bs,
+            'n_epochs'  : epochs.at[bs,lr],
+            'optimizer' : "Adam",
+            'lr'        : lr,
+            'loss'      : loss 
+        }
+
+        #print("\n\ttraining network...\n")
+        count_try = 0
+        while (info == "try again" and count_try < opts["max_try"]) or count_try == 0 :
 
             if info == "try again":
-                print("\nAborted training. Let's go on!\n") 
+                print("\nLet's try again\n")
 
-            df.at[n,"file"] = df.at[n,"file"] + ".pdf"
+            model, arrays, corr, info = \
+                train(  model=net,
+                        train_dataset=train_dataset,
+                        val_dataset=val_dataset,
+                        hyperparameters=hyperparameters,
+                        get_pred=net.get_pred,
+                        get_real=net.get_real, #lambda X: net.get_real(X=X,output=net.output),
+                        output=parameters["output_folder"],
+                        # correlation = net.correlation,
+                        name=df.at[n,"file"],
+                        opts=opts,
+                        parameters=parameters)
+            count_try += 1
+
+        if info == "try again":
+            print("\nAborted training. Let's go on!\n") 
+
+        df.at[n,"file"] = df.at[n,"file"] + ".pdf"
+        n += 1
+
+        df[:n].to_csv("temp-info.csv",index=False)
+
+        return info
+    
+    ##########################################
+    # looping over all hyper-parameters
+    n = 0 
+    init_model = deepcopy(net) 
+    for bs,lr in hyper_pars :
+        if parameters["trial"] is not None : 
+            for n_trial in parameters["trial"] : 
+                run(n,bs,lr)
+                n += 1
+        else :       
+            run(n,bs,lr)
             n += 1
 
-            df[:n].to_csv("temp-info.csv",index=False)
+    ##########################################
+    # remove EXIT file if detected
+    if os.path.exists("EXIT"):
+        os.remove("EXIT")
+        print("\n\t'exit' file detected\n")
 
-    # write information to file 'info.csv'
+    ##########################################
+    # finish
     try : 
+        # write information to file 'info.csv'
         df.to_csv("info.csv",index=False)
 
         # remove 'temp-info.csv'

miscellaneous/elia/nn/iPIinterface.py
@@ -3,12 +3,18 @@ from ase.io import read
 from miscellaneous.elia.nn.dataset.make_dataset_delta import make_datapoint_delta
 from miscellaneous.elia.nn.dataset import make_datapoint
 from miscellaneous.elia.good_coding import froze
+from .SabiaNetwork import SabiaNetwork
+from typing import Dict, Union
+import torch_geometric
+from torch_geometric.data import Data
 # from torch_geometric.loader import DataLoader
 # from torch_geometric.data import Data
 from abc import ABC, abstractproperty
+from typing import TypeVar
+T = TypeVar('T', bound='iPIinterface')
 
 #@froze
-class iPIinterface():
+class iPIinterface(SabiaNetwork):
 
     # __slots__ = ("normalization","_max_radius","_symbols")
 
@@ -24,23 +30,30 @@ class iPIinterface():
     # def reference(self): 
     #     pass
 
-    def __init__(self,max_radius:float,normalization:dict=None,**kwargs):
+    def __init__(self:T,max_radius:float,normalization:dict=None,**kwargs):
 
         super().__init__(max_radius=max_radius,**kwargs)
 
         if normalization is None :
             normalization = {
-                "energy":{
-                    "mean" : 0.,
-                    "std"  : 1.,
-                },
-                "dipole":{
-                    "mean" : 0.,# torch.tensor([0.,0.,0.]),
-                    "std"  : 1.,# torch.tensor([1.,1.,1.]),
-                },
-            }
+                    "mean" : None,
+                    "std"  : None,
+                }
+            # normalization = {
+            #     "energy":{
+            #         "mean" : 0.,
+            #         "std"  : 1.,
+            #     },
+            #     "dipole":{
+            #         "mean" : 0.,# torch.tensor([0.,0.,0.]),
+            #         "std"  : 1.,# torch.tensor([1.,1.,1.]),
+            #     },
+            # }
+
+        self._mean = torch.nn.Parameter(torch.tensor(normalization["mean"]))
+        self._std  = torch.nn.Parameter(torch.tensor(normalization["std"]))
         
-        self.normalization = normalization
+        # self.normalization = normalization
         # for k in self.normalization.keys():
         #     for j in self.normalization[k].keys():
         #         x = self.normalization[k][j]
@@ -50,6 +63,10 @@ class iPIinterface():
         self._symbols = None
 
         pass
+
+    def forward(self:T,data: Union[torch_geometric.data.Data, Dict[str, torch.Tensor]])-> torch.Tensor:
+        y = super().forward(data)
+        return y * self._std + self._mean
 
     def make_datapoint(self,lattice, positions,**argv):
 
@@ -75,55 +92,57 @@ class iPIinterface():
 
         pass
     
-    def _get(self,X,what:str,**argv)-> torch.tensor:
-        """Get the correct value of the output restoring the original 'mean' and 'std' values.
-        This should be used only during MD simulation."""
+    # def _get(self,X,what:str,**argv)-> torch.tensor:
+    #     """Get the correct value of the output restoring the original 'mean' and 'std' values.
+    #     This should be used only during MD simulation."""
 
-        # lower case
-        what = what.lower()
+    #     # lower case
+    #     what = what.lower()
 
-        # compute output of the model
-        if what == "energy" :
-            y = self.energy(X,**argv)
+    #     # compute output of the model
+    #     if what == "energy" :
+    #         y = self.energy(X,**argv)
         
-        elif what == "forces":
-            y = self.forces(X,**argv)
+    #     elif what == "forces":
+    #         y = self.forces(X,**argv)
         
-        elif what == "dipole":
-            y = self.dipole(X,**argv)
+    #     elif what == "dipole":
+    #         y = self.dipole(X,**argv)
         
-        elif what == "bec":
-            y = self.BEC(X,**argv)
-        else :
-            raise ValueError("quantity '{:s}' is not supported as output of this model".format(what))
+    #     elif what == "bec":
+    #         y = self.BEC(X,**argv)
+    #     else :
+    #         raise ValueError("quantity '{:s}' is not supported as output of this model".format(what))
 
-        mean = None
-        std = None
-        if what == "energy" :
-            mean, std = self.normalization["energy"]["mean"], self.normalization["energy"]["std"]
+    #     mean = None
+    #     std = None
+    #     if what == "energy" :
+    #         mean, std = self.normalization["energy"]["mean"], self.normalization["energy"]["std"]
         
-        elif what == "forces":
-            std = self.normalization["energy"]["std"]
+    #     elif what == "forces":
+    #         std = self.normalization["energy"]["std"]
         
-        elif what == "dipole":
-            mean, std = self.normalization["dipole"]["mean"], self.normalization["dipole"]["std"]
+    #     elif what == "dipole":
+    #         mean, std = self.normalization["dipole"]["mean"], self.normalization["dipole"]["std"]
         
-        elif what == "bec":
-            std = self.normalization["dipole"]["std"]
+    #     elif what == "bec":
+    #         std = self.normalization["dipole"]["std"]
 
-        # resize
-        # batch_size = y.shape[0]
-        # newdim = [1]*(len(y.shape)-2) # I remove the batch_size axis and the 'actual' value of the output
-        # mean = mean.view(batch_size,3,*newdim) if mean is not None else mean
-        # std  =  std.view(batch_size,3,*newdim) if  std is not None else std
+    #     # resize
+    #     # batch_size = y.shape[0]
+    #     # newdim = [1]*(len(y.shape)-2) # I remove the batch_size axis and the 'actual' value of the output
+    #     # mean = mean.view(batch_size,3,*newdim) if mean is not None else mean
+    #     # std  =  std.view(batch_size,3,*newdim) if  std is not None else std
 
-        if what in ["energy","dipole"] :
-            return y * std + mean
+    #     if what in ["energy","dipole"] :
+    #         return y * std + mean
         
-        elif what in ["forces","bec"]:
-            return y * std
+    #     elif what in ["forces","bec"]:
+    #         return y * std
         
     def get(self,cell,pos,what:str,detach=True,**argv):
+
+        self.eval()
 
         requires_grad = {   "pos"        : True,\
                             "lattice"    : True,\
@@ -133,7 +152,11 @@ class iPIinterface():
 
         X = self.make_datapoint(lattice=cell.T,positions=pos,requires_grad=requires_grad)
 
-        y = self._get(what=what,X=X,**argv)
+        # y = self._get(what=what,X=X,**argv)
+        if what.lower() not in ["bec","forces"]:
+            y = self(X)
+        else :
+            raise ValueError("not implemented yet")
 
         if detach :
             y = y.detach()

miscellaneous/elia/nn/make_dataset_delta.py
@@ -1,12 +1,12 @@
 # Import necessary modules and classes
-from miscellaneous.elia.nn.dataset import make_dataset, make_datapoint
+from miscellaneous.elia.nn.make_dataset import make_dataset, make_datapoint
 import torch
 from torch_geometric.data import Data
 
 #----------------------------------------------------------------#
 # Function to add a reference to a datapoint
 def add_reference(datapoint: Data,
-                  #dipole: torch.tensor,
+                  dipole: torch.tensor,
                   pos: torch.tensor):
     """
     Adds a reference to a datapoint by subtracting dipole and adding deltaR.
@@ -50,14 +50,14 @@ def make_dataset_delta(ref_index: int = 0, **argv):
     dataset = make_dataset(**argv)
 
     # Clone the dipole and position of the reference datapoint
-    # dipole = dataset[ref_index].dipole.clone()
+    dipole = dataset[ref_index].dipole.clone()
     pos = dataset[ref_index].pos.clone()
 
     # Add references to each datapoint in the dataset
     for n in range(len(dataset)):
-        dataset[n] = add_reference(dataset[n], pos)
+        dataset[n] = add_reference(dataset[n], dipole, pos)
 
-    return dataset, pos
+    return dataset, dipole, pos
 
 #----------------------------------------------------------------#
 

miscellaneous/elia/nn/prepare_dataset.py
@@ -105,14 +105,14 @@ def prepare_dataset(ref_index:int,\
         if os.path.exists(savefile+".torch") and not RESTART:
             dataset = torch.load(savefile+".torch")
             if reference:
-                dipole  = dataset[ref_index].dipole
+                # dipole  = dataset[ref_index].dipole
                 pos     = dataset[ref_index].pos
             else :
-                dipole = torch.full((3,),torch.nan)
+                # dipole = torch.full((3,),torch.nan)
                 pos = torch.full((3,),torch.nan)
         else :
             if reference :
-                dataset, dipole, pos = make_dataset_delta(  ref_index = ref_index,
+                dataset, pos = make_dataset_delta(  ref_index = ref_index,
                                                             data = data,
                                                             max_radius = max_radius,\
                                                             output=output,\
@@ -122,14 +122,14 @@ def prepare_dataset(ref_index:int,\
                                                 max_radius = max_radius,\
                                                 output=output,\
                                                 requires_grad = requires_grad)
-                dipole = torch.full((3,),torch.nan)
+                # dipole = torch.full((3,),torch.nan)
                 pos = torch.full((3,),torch.nan)
             else :
                 dataset = make_dataset( data=data,\
                                         max_radius=max_radius,\
                                         output=output,\
                                         requires_grad=requires_grad)
-                dipole = torch.full((3,),torch.nan)
+                # dipole = torch.full((3,),torch.nan)
                 pos = torch.full((3,),torch.nan)
         # shuffle
         random.shuffle(dataset)
@@ -157,10 +157,10 @@ def prepare_dataset(ref_index:int,\
             # Open the JSON file and load the data
             with open("{:s}/reference.json".format(folder)) as f:
                 reference = json.load(f)
-            dipole = torch.tensor(reference['dipole'])
+            # dipole = torch.tensor(reference['dipole'])
             pos    = torch.tensor(reference['pos'])
         else :
-            dipole = torch.full((3,),torch.nan)
+            # dipole = torch.full((3,),torch.nan)
             pos = torch.full((3,),torch.nan)
 
         SAVE = False
@@ -175,7 +175,7 @@ def prepare_dataset(ref_index:int,\
             # Write the dictionary to the JSON file
             with open("reference.json", "w") as json_file:
                 # The 'indent' parameter is optional for pretty formatting
-                json.dump({"dipole":dipole.tolist(),"pos":pos.tolist()}, json_file, indent=4)  
+                json.dump({"pos":pos.tolist()}, json_file, indent=4)  
 
     print("\n\tDatasets summary:")
     print("\t\ttrain:",len(train_dataset))
@@ -192,4 +192,4 @@ def prepare_dataset(ref_index:int,\
 
     example = Atoms(positions=pos,cell=cell,symbols=symbols)
 
-    return datasets, data, dipole, pos, example
+    return datasets, data, pos, example

miscellaneous/elia/nn/train-e3nn-model.py
@@ -1,7 +1,12 @@
+import time
+start_time = time.time()
+
 import numpy as np
 import random
 import json
 import argparse
+from copy import copy
+
 import torch
 default_dtype = torch.float64
 torch.set_default_dtype(default_dtype)
@@ -12,6 +17,7 @@ from miscellaneous.elia.nn.prepare_dataset import prepare_dataset
 from miscellaneous.elia.nn.normalize_datasets import normalize_datasets
 from miscellaneous.elia.nn import SabiaNetworkManager
 from miscellaneous.elia.functions import add_default, args_to_dict, str2bool
+from miscellaneous.elia.nn import compute_normalization_factors
 
 #----------------------------------------------------------------#
 # Documentation
@@ -43,6 +49,10 @@ default_values = {
         "epochs"        : 10000,
         "bs"            : [1],
         "lr"            : [1e-3],
+        "grid"          : True,
+        "trial"         : None,
+        "max_time"      : -1,
+        "task_time"     : -1
     }
 
 #####################
@@ -54,19 +64,25 @@ def get_args():
 
     # Argument for "input"
     parser.add_argument(
-        "-i", "--input", action="store", type=int, metavar="\bjson_file",
+        "-i", "--input", action="store", type=str, metavar="\bjson_file",
         help="input json file", default=None
+    )
+
+    # Argument for "input"
+    parser.add_argument(
+        "--options", action="store", type=str, metavar="\boptions_file",
+        help="options json file", default=None
     )
 
     # Argument for "mul"
     parser.add_argument(
-        "-m", "--mul", action="store", type=int, metavar="\bmultiplicity",
+        "--mul", action="store", type=int, metavar="\bmultiplicity",
         help="multiplicity for each node (default: 2)", default=default_values["mul"]
     )
 
     # Argument for "layers"
     parser.add_argument(
-        "-l", "--layers", action="store", type=int, metavar="\bn_layers",
+        "--layers", action="store", type=int, metavar="\bn_layers",
         help="number of layers (default: 6)", default=default_values["layers"]
     )
 
@@ -153,8 +169,36 @@ def get_args():
 
     # Argument for "lr"
     parser.add_argument(
-        "--lr", action="store", type=float, nargs="+", metavar="\blr",
+        "--lr", action="store", type=float, nargs="+", metavar="\blearning_rate",
         help="some description here (default: [1e-3])", default=default_values["lr"]
+    )
+
+    # Argument for "reference"
+    parser.add_argument(
+        "--grid", action="store",type=str2bool, metavar="\bas_grid",
+        help="some description here (default: True)",
+        default=default_values["grid"]
+    )
+
+    # Argument for "reference"
+    parser.add_argument(
+        "--trial", action="store",type=int, metavar="\bn_trial",
+        help="some description here (default: True)",
+        default=default_values["trial"]
+    )
+
+    # Argument for "max_time"
+    parser.add_argument(
+        "--max_time", action="store",type=int, metavar="\bmax_time",
+        help="some description here (default: True)",
+        default=default_values["max_time"]
+    )
+
+    # Argument for "task_time"
+    parser.add_argument(
+        "--task_time", action="store",type=int, metavar="\btask_time",
+        help="some description here (default: True)",
+        default=default_values["task_time"]
     )
 
     return parser.parse_args()
@@ -162,6 +206,16 @@ def get_args():
 #####################
 
 def check_parameters(parameters):
+    
+    str2bool_keys = ["reference","phases","random","grid"]
+    for k in str2bool_keys : 
+        parameters[k] = str2bool(parameters[k])
+    
+    if parameters["task_time"] <= 0 :
+        parameters["task_time"] = -1
+
+    if parameters["max_time"] <= 0 :
+        parameters["max_time"] = -1
 
     if parameters["reference"] and parameters["phases"]:
         raise ValueError("You can use 'reference'=true or 'phases'=true, not both.")
@@ -173,6 +227,8 @@ def check_parameters(parameters):
 
 def main():
 
+    start_time 
+
     ##########################################
     # get user parameters
 
@@ -181,7 +237,9 @@ def main():
     if args.input is not None :
         # read parameters from file
         try :
-            parameters = json.load(args.input)
+            with open(args.input, 'r') as file:
+                parameters = json.load(file)
+
         except :
             raise ValueError("error reading input file")
         # it should not be needed ...
@@ -217,7 +275,7 @@ def main():
     ##########################################
     # preparing dataset
     opts = {"prepare":{"restart":False},"build":{"restart":False}}
-    datasets, data, dipole, pos, example = prepare_dataset(ref_index=parameters["ref_index"],\
+    datasets, data, pos, example = prepare_dataset(ref_index=parameters["ref_index"],\
                                                   max_radius=parameters["max_radius"],\
                                                   reference=parameters["reference"],\
                                                   output=parameters["output"],\
@@ -241,7 +299,35 @@ def main():
 
     ##########################################
     # normalizing dataset
-    normalization_factors, datasets = normalize_datasets(datasets)
+    normalization_factors = {
+        "dipole":{
+            "mean":0,
+            "std":1
+        },
+        "energy":{
+            "mean":0,
+            "std":1
+        }
+    }
+
+    if "D" in parameters["output"] :
+        mean, std = compute_normalization_factors(datasets["train"],"dipole")
+        normalization_factors["dipole"] = {"mean":mean,"std":std}
+
+    if "E" in parameters["output"] :
+        mean, std = compute_normalization_factors(datasets["train"],"energy")
+        normalization_factors["energy"] = {"mean":mean,"std":std}
+
+    # normalization_factors, datasets = normalize_datasets(datasets)
+
+    match parameters["output"] :
+        case "D" :
+            normalization_factors = normalization_factors["dipole"]
+        case ["E","F"] :
+            normalization_factors = normalization_factors["energy"]
+        case _ :
+            raise ValueError("not implemented yet")
+
 
     ##########################################
     # visualize dataset
@@ -297,7 +383,7 @@ def main():
         "reference" : parameters["reference"],
         "phases" : parameters["phases"],
         "normalization" : normalization_factors,
-        "dipole" : dipole.tolist(),
+        # "dipole" : dipole.tolist(),
         "pos" : pos.tolist(),  
     }
 
@@ -320,12 +406,13 @@ def main():
     kwargs = {**metadata_kwargs, **model_kwargs}
 
     instructions = {
-            "kwargs":kwargs,
+            "kwargs":copy(kwargs),
             "class":"SabiaNetworkManager",
             "module":"miscellaneous.elia.nn",
             "chemical-symbols" : example.get_chemical_symbols()
         }
     
+    del instructions["kwargs"]["normalization"]
     with open("instructions.json", "w") as json_file:
         # The 'indent' parameter is optional for pretty formatting
         json.dump(instructions, json_file, indent=4)
@@ -334,7 +421,10 @@ def main():
     print(net)
     N = 0 
     for i in net.parameters():
-        N += len(i)
+        if len(i.shape) != 0 :
+            N += len(i)
+        else :
+            N += 1
     print("Tot. number of parameters: ",N)
 
     ##########################################
@@ -347,24 +437,40 @@ def main():
     ##########################################
     # optional settings
 
-    if parameters["Natoms"] is None :
-        parameters["Natoms"] = example.get_number_of_atoms()
+    if parameters["Natoms"] is None or parameters["Natoms"] == 'None' :
+        parameters["Natoms"] = example.get_global_number_of_atoms()
 
     opts = {
-            "name" : parameters["name"],
+            #"name" : parameters["name"],
             "plot":{
                 "learning-curve" : {"N":10},
                 "correlation" : {"N":10}
             },
             "thr":{
-                "exit":1e2
+                "exit":-1
             },
-            "Natoms" : parameters["Natoms"] ,
-            "output_folder" : parameters["output_folder"],
+            #"Natoms" : parameters["Natoms"] ,
+            #"output_folder" : parameters["output_folder"],
             "save":{
                 "parameters":1,
-            }
+                "checkpoint":1,
+            },
+            #"grid" : parameters["grid"],
+            #"trial" : parameters["trial"],
+            "start_time" : start_time,
+            'keep_dataset' : True,
         }
+
+    if args.options is not None :
+        # read parameters from file
+        try :
+            with open(args.options, 'r') as file:
+                options = json.load(file)
+
+        except :
+            raise ValueError("error reading options file")
+        # it should not be needed ...
+        opts = add_default(options,opts)
 
     ##########################################
     # hyper-train the model
@@ -374,7 +480,8 @@ def main():
                                 epochs   = parameters["epochs"],\
                                 loss     = loss,\
                                 datasets = datasets,\
-                                opts     = opts )
+                                opts     = opts,\
+                                parameters = parameters)
 
     print("\nJob done :)")
 

miscellaneous/elia/nn/train.py
@@ -7,16 +7,26 @@ from tqdm import tqdm
 import pandas as pd
 import warnings
 import os
+import shutil
 from copy import copy
 from .make_dataloader import _make_dataloader
 from ..functions import add_default, remove_empty_folder
 from .plot import plot_learning_curves
+import time
 
 __all__ = ["train"]
 
+yval_real = None 
+all_dataloader_val   = None
+
+ytrain_real = None
+all_dataloader_train = None
+
+#@torch.jit.script
 def train(model:torch.nn.Module,\
           train_dataset:list,\
           val_dataset:list,\
+          parameters:dict,\
           hyperparameters:dict=None,\
           get_pred:callable=None,\
           get_real:callable=None,\
@@ -65,7 +75,8 @@ def train(model:torch.nn.Module,\
                 'val_loss'  : array with the average loss (mean over the mini-batches) of the validation dataset
             Each element of these arrays is referred to one epoch. 
     """
-    
+    start_task_time = time.time()
+
     print("\nTraining:")
     print("\tPreparing training")
 
@@ -76,7 +87,7 @@ def train(model:torch.nn.Module,\
                 "dataloader":{"shuffle":False},\
                 "thr":{"exit":10000},\
                 "disable":False,\
-                "Natoms":1,\
+                #"Natoms":1,\
                 "save":{"parameters":1}} # ,"networks-temp":-1
     opts = add_default(opts,default)
 
@@ -228,14 +239,18 @@ def train(model:torch.nn.Module,\
     arrays = pd.DataFrame({ "train":train_loss,\
                             "val":val_loss})
 
+    global yval_real, all_dataloader_val
     # compute the real values of the validation dataset only once
-    print("\tCompute validation dataset output (this will save time in the future)")
-    yval_real   = get_all(val_dataset)
-    all_dataloader_val   = get_all_dataloader(val_dataset)
+    if yval_real is None or not opts["keep_dataset"]:
+        print("\tCompute validation dataset output (this will save time in the future)")
+        yval_real   = get_all(val_dataset)
+        all_dataloader_val   = get_all_dataloader(val_dataset)
 
-    print("\tCompute training dataset output (this will save time in the future)")
-    ytrain_real = get_all(train_dataset)
-    all_dataloader_train = get_all_dataloader(train_dataset)
+    global ytrain_real, all_dataloader_train
+    if ytrain_real is None or not opts["keep_dataset"]:
+        print("\tCompute training dataset output (this will save time in the future)")
+        ytrain_real = get_all(train_dataset)
+        all_dataloader_train = get_all_dataloader(train_dataset)
 
     savefile = "{:s}/{:s}.init.torch".format(folders["networks"],name)
     print("\tSaving 'model' with dummy parameters to file '{:s}'".format(savefile))
@@ -245,14 +260,79 @@ def train(model:torch.nn.Module,\
     corr = None
     if correlation is not None:        
         corr = pd.DataFrame(columns=["train","val"],index=np.arange(n_epochs))
-    
+
+    ##########################################
+    # prepare output files
+    savefiles = {
+        "dataframes"   : "{:s}/{:s}.csv".format(folders["dataframes"],name),
+        "correlations" : "{:s}/{:s}.csv".format(folders["correlations"],name),
+        "images"       : "{:s}/{:s}.pdf".format(folders["images"],name),
+    }
+
+    ##########################################
+    # prepare checkpoint
+    # https://pytorch.org/tutorials/recipes/recipes/saving_and_loading_a_general_checkpoint.html 
+
+    start_epoch = 0 
+
+    checkpoint_folder = "checkpoint"
+    if not os.path.exists(checkpoint_folder):
+        os.mkdir(checkpoint_folder)
+
+    checkpoint_file = "{:s}/{:s}.pth".format(checkpoint_folder,name)
+    if os.path.exists(checkpoint_file):
+        print("\tReading checkpoint from file '{:s}'".format(checkpoint_file))
+        checkpoint = torch.load(checkpoint_file)
+
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        start_epoch = checkpoint['epoch']
+        # loss = checkpoint['loss']
+
+        # read array and csv
+        if os.path.exists(savefiles["dataframes"]):
+            arrays = pd.read_csv(savefiles["dataframes"])
+        if os.path.exists(savefiles["correlations"]):
+            corr   = pd.read_csv(savefiles["correlations"])
+
+    def save_checkpoint():
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+        }, checkpoint_file)
+
+    ##########################################    
     # start the training procedure
     print("\n\t...and here we go!")
-    for epoch in range(n_epochs):    
+    for epoch in range(start_epoch,n_epochs):    
 
         if info != "all good":
             break
-        
+
+        ##########################################
+        # measure elapsed time
+        now = time.time()
+        if abs(now - opts["start_time"]+60) > parameters["max_time"] and parameters["max_time"] > 0 :
+            print("\n\tMaximum time reached: stopping")
+            info = "time over"
+            break
+        if abs(now - start_task_time + 60 ) > parameters["task_time"] and parameters["task_time"] > 0 :
+            print("\n\tMaximum time per task reached: stopping task")
+            info = "time over"
+            break
+
+        if os.path.exists("EXIT"):
+            info = "exit file detected"
+            break
+
+        if os.path.exists("EXIT-TASK"):
+            os.remove("EXIT-TASK")
+            info = "exit-task file detected"
+            break
+
+        ##########################################
+        # training loop per epoch
         with tqdm(enumerate(dataloader_train),\
                   total=batches_per_epoch,\
                   bar_format='{l_bar}{bar:10}{r_bar}{bar:-10b}',\
@@ -273,7 +353,7 @@ def train(model:torch.nn.Module,\
                 loss = loss_fn(y_pred,y_real)
 
                 # store the loss function in an array
-                train_loss_one_epoch[step] = float(loss)/opts["Natoms"]
+                train_loss_one_epoch[step] = float(loss)/parameters["Natoms"]
 
                 # backward pass
                 optimizer.zero_grad()
@@ -300,6 +380,8 @@ def train(model:torch.nn.Module,\
             # maybe it inferferes with the calculation of the forces
             # model.eval()
             with torch.no_grad():
+
+                model.eval()
                 
                 # saving model to temporary file
                 # N = opts["save"]["networks-temp"]
@@ -316,13 +398,13 @@ def train(model:torch.nn.Module,\
                 # compute the loss function
                 # predict the value for the validation dataset
                 yval_pred = get_pred(all_dataloader_val)# get_pred(model,all_dataloader_val)
-                val_loss[epoch] = float(loss_fn(yval_pred,yval_real))/opts["Natoms"]
+                val_loss[epoch] = float(loss_fn(yval_pred,yval_real))/parameters["Natoms"]
 
                 # set arrays
                 ytrain_pred = get_pred(X=all_dataloader_train) # get_pred(model=model,X=all_dataloader_train)
-                train_loss[epoch] = float(loss_fn(ytrain_pred,ytrain_real))/opts["Natoms"]
+                train_loss[epoch] = float(loss_fn(ytrain_pred,ytrain_real))/parameters["Natoms"]
 
-                if train_loss[epoch] > opts["thr"]["exit"]:
+                if train_loss[epoch] > opts["thr"]["exit"] and opts["thr"]["exit"] > 0:
                     info = "try again"
                     break
 
@@ -331,8 +413,8 @@ def train(model:torch.nn.Module,\
                 arrays.at[epoch,"val"  ] = val_loss  [epoch]
 
                 # save loss to file
-                savefile =  "{:s}/{:s}.csv".format(folders["dataframes"],name)
-                arrays.to_csv(savefile,index=False)
+                # savefile = "{:s}/{:s}.csv".format(folders["dataframes"],name)
+                arrays.to_csv(savefiles["dataframes"],index=False)
 
                 if correlation is not None :
                     # compute correlation
@@ -341,15 +423,15 @@ def train(model:torch.nn.Module,\
                     corr["val"][epoch] = correlation(yval_pred, yval_real)
 
                     # save correlation to file
-                    savefile =  "{:s}/{:s}.csv".format(folders["correlations"],name)
-                    corr.to_csv(savefile,index=False)
+                    # savefile =  "{:s}/{:s}.csv".format(folders["correlations"],name)
+                    corr.to_csv(savefiles["correlations"],index=False)
 
                 # produce learning curve plot
                 if epoch > 1:
-                    savefile =  "{:s}/{:s}.pdf".format(folders["images"],name)
+                    # savefile =  "{:s}/{:s}.pdf".format(folders["images"],name)
                     plot_learning_curves(train_loss[:epoch+1],\
                                         val_loss[:epoch+1],\
-                                        file=savefile,\
+                                        file=savefiles["images"],\
                                         title=name if name != "untitled" else None,\
                                         opts=opts["plot"]["learning-curve"])
 
@@ -364,6 +446,14 @@ def train(model:torch.nn.Module,\
                                     corr_train=corr["train"][epoch],
                                     val=val_loss[epoch],
                                     corr_val=corr["val"][epoch])
+        
+        # saving checkpoint to file
+        N = opts["save"]["checkpoint"]
+        if N != -1 and epoch % N == 0 :
+            save_checkpoint()
+        
+    #
+    save_checkpoint()
     
     # Finished training 
     #print("\n\tTraining done!")
@@ -371,6 +461,13 @@ def train(model:torch.nn.Module,\
     # Saving some quantities to file
     if info == "all good":
         
+        # modify checkpoint filename
+        if os.path.exists(checkpoint_file):
+            new_file = "{:s}/finished-{:s}.pth".format(checkpoint_folder,name)
+            os.rename(checkpoint_file,new_file)
+        else :
+            save_checkpoint()
+
         # saving model to file
         savefile = "{:s}/{:s}.torch".format(folders["networks"],name)
         print("\tSaving 'model' to file '{:s}'".format(savefile))
@@ -407,5 +504,7 @@ def train(model:torch.nn.Module,\
     # We will let the user know about that through the variable 'info'
     elif info == "try again":
         print("\n\tTraining stopped: we could try again\n")
+    elif info == "exit-task file detected":
+        print("\n\t'exit-task' file detected\n")
     
     return model, arrays, corr, info