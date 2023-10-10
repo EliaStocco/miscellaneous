f57ece8f02b612d214a2f7d412ac92edb38b24df
EliaStocco <eliastocco@gmail.com>
2023-10-10 11:51:08 +0200

    added I/O formats

--- miscellaneous/elia/convert-file.py
+++ miscellaneous/elia/convert-file.py
@@ -1,5 +1,6 @@
 from ase.io import read, write
-from miscellaneous.elia.functions import str2bool, suppress_output
+from miscellaneous.elia.functions import str2bool, suppress_output, convert
+# from miscellaneous.elia.classes import MicroState
 import argparse
 
 # Description of the script's purpose
@@ -13,15 +14,18 @@ parser.add_argument("-o" , "--output"       , action="store", type=str     , hel
 parser.add_argument("-if", "--input_format" , action="store", type=str     , help="input file format (default: 'None')" , default=None)
 parser.add_argument("-of", "--output_format", action="store", type=str     , help="output file format (default: 'None')", default=None)
 parser.add_argument("-d" , "--debug"        , action="store", type=str2bool, help="debug (default: False)"              , default=False)
+parser.add_argument("-iu" , "--input_unit"  , action="store", type=str     , help="unit of the input file positions (default: atomic_unit)"  , default=None)
+parser.add_argument("-ou" , "--output_unit" , action="store", type=str     , help="unit of the output file positions (default: atomic_unit)", default=None)
 
-args = parser.parse_args()
-end = "" if not args.debug else ""
 
 # Print the script's description
 print("\n\t{:s}".format(description))
 
 # Parse the command-line arguments
-print("\n\tRead input arguments ... ")
+print("\n\tReading input arguments ... ",end="")
+args = parser.parse_args()
+end = "" if not args.debug else ""
+print("done")
 
 
 # Read the MicroState data from the input file
@@ -35,6 +39,14 @@ print("\tReading data from input file '{:s}' ... ".format(args.input), end=end)
 with suppress_output(not args.debug):
     atoms = read(args.input,format=args.input_format)
 if not args.debug:
+    print("done")
+
+if args.output_unit is not None :
+    if args.input_unit is None :
+        args.input_unit = "atomic_unit"
+    print("\tConverting positions from '{:s}' to {:s} ... ".format(args.input_unit,args.output_unit), end=end)
+    factor = convert(what=1,family="length",_to=args.output_unit,_from=args.input_unit)
+    atoms.positions *= factor
     print("done")
 
 # Write the data to the specified output file with the specified format
