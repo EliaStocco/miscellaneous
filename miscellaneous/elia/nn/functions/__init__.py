b540b8badf66e96ea4733c684f77ea823241e585
EliaStocco <eliastocco@gmail.com>
2023-11-23 13:38:24 +0100

    fixed problem with import

--- miscellaneous/elia/nn/functions/__init__.py
+++ miscellaneous/elia/nn/functions/__init__.py
@@ -1,6 +1,6 @@
-from .functions import get_data_from_dataset as get_data_from_dataset
-from .functions import get_model as get_model
-from .functions import count_parameters as count_parameters
-from .functions import get_function as get_function
-from .functions import bash_as_function as bash_as_function
-from .moving_average import MovingAverage as MovingAverage
+# from .functions import get_data_from_dataset as get_data_from_dataset
+# from .functions import get_model as get_model
+# from .functions import count_parameters as count_parameters
+# from .functions import get_function as get_function
+# from .functions import bash_as_function as bash_as_function
+# from .moving_average import MovingAverage as MovingAverage
