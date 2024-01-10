3ec201f04ca37f9850f307def1b541cd14cab9cb
EliaStocco <eliastocco@gmail.com>
2024-01-10 17:20:47 +0100

    small changes

--- miscellaneous/elia/nn/scripts/fps.py
+++ miscellaneous/elia/nn/scripts/fps.py
@@ -3,6 +3,7 @@ from ase.io import read, write
 import chemiscope
 import numpy as np
 import json
+import os
 
 from rascal.representations import SphericalInvariants as SOAP
 from skmatter.preprocessing import StandardFlexibleScaler
@@ -11,23 +12,27 @@ from sklearn.decomposition import PCA
 from tqdm.auto import tqdm
 import argparse
 from miscellaneous.elia.functions import add_default
+from miscellaneous.elia.show import show_dict
+from miscellaneous.elia.functions import convert
 
 def main():
 
     description = "Process atomic structures and select a diverse subset using the Farthest Point Sampling (FPS) algorithm."
 
-    message = "!Pay attention that the provided positions should be in angstrom!"
+    message = "!Pay attention that the provided positions should be in atomic unit!"
 
     # Define the command-line argument parser with a description
     parser = argparse.ArgumentParser()
     argv = {"metavar" : "\b"}
-    parser.add_argument("-i" , "--input"         , action="store", type=str , **argv, help="input file")
-    parser.add_argument("-o" , "--output"        , action="store", type=str , **argv, help="output file with the selected structures ")
-    parser.add_argument("-oi", "--output_indices", action="store", type=str , **argv, help="output file with indices of the selected structures (default: 'indices.txt')",default='indices.txt')
-    parser.add_argument("-if", "--input_format"  , action="store", type=str , **argv, help="input file format (default: 'None')" , default=None)
-    parser.add_argument("-of", "--output_format" , action="store", type=str , **argv, help="output file format (default: 'None')", default=None)
-    parser.add_argument("-n" , "--number"        , action="store", type=int , **argv, help="number of desired structure (default: '100')", default=None)
-    parser.add_argument("-sh", "--soap_hyper"    , action="store", type=str , **argv, help="JSON file with the SOAP hyperparameters", default=None)
+    parser.add_argument("-i" , "--input"          ,  type=str  , **argv, help="input file")
+    parser.add_argument("-o" , "--output"         ,  type=str  , **argv, help="output file with the selected structures ")
+    parser.add_argument("-oi", "--output_indices" ,  type=str  , **argv, help="output file with indices of the selected structures (default: 'indices.txt')",default='indices.txt')
+    parser.add_argument("-if", "--input_format"   ,  type=str  , **argv, help="input file format (default: 'None')" , default=None)
+    parser.add_argument("-of", "--output_format"  ,  type=str  , **argv, help="output file format (default: 'None')", default=None)
+    parser.add_argument("-ff", "--features_file"  ,  type=str  , **argv, help="features output file [*.npy] (default: 'None')", default=None)
+    parser.add_argument("-n" , "--number"         ,  type=int  , **argv, help="number of desired structure (default: '100')", default=None)
+    parser.add_argument("-rc", "--cutoff_radius"  ,  type=float, **argv, help="cutoff radius in atomic unit (default: 6)", default=6)
+    parser.add_argument("-sh", "--soap_hyper"     ,  type=str  , **argv, help="JSON file with the SOAP hyperparameters", default=None)
 
     # Print the script's description
     print("\n\t{:s}".format(description))
@@ -42,6 +47,15 @@ def main():
     #
     print("\n\tReading positions from file '{:s}' ... ".format(args.input),end="")
     frames = read(args.input, index=':', format=args.input_format)  #eV
+    print("done")
+
+    print("\n\tConverting the atomic structures positions and cells from atomic unit to angstrom ... ",end="")
+    factor = convert(1,"length","atomic_unit","angstrom")
+    for n in range(len(frames)):
+        frames[n].positions *= factor
+        if np.any(frames[n].pbc):
+            cell = factor * frames[n].get_cell()
+            frames[n].set_cell(cell)
     print("done")
 
     available_structure_properties = list(set([k for frame in frames for k in frame.info.keys()]))
@@ -61,10 +75,9 @@ def main():
     else:
         user_soap_hyper = None
 
-
-    print("\n\tPreparing SOAP object ... ",end="")
+    print("\n\tUsing the following SOAP hyperparameters:")
     SOAP_HYPERS = {
-        "interaction_cutoff": 3.5,
+        # "interaction_cutoff": 3.5,
         "max_radial": 8,
         "max_angular": 6,
         "gaussian_sigma_constant": 0.4,
@@ -73,8 +86,11 @@ def main():
     }
 
     SOAP_HYPERS = add_default(user_soap_hyper,SOAP_HYPERS)
+    SOAP_HYPERS["interaction_cutoff"] = args.cutoff_radius
+    show_dict(SOAP_HYPERS,string="\t\t")
 
     #
+    print("\n\tPreparing SOAP object ... ",end="")
     numbers = list(sorted(set([int(n) for frame in frames for n in frame.numbers])))
 
     # initialize SOAP
@@ -100,13 +116,15 @@ def main():
             X = np.zeros((len(frames), x.shape[-1]))
         X[i] = x
 
-    print(f"\tSOAP features shape: {X.shape}")
+    print(f"\n\tSOAP features shape: {X.shape}")
 
-    file = 'full-featurization.npy'
-    print("\tSaving features to file '{:s}' ... ".format(file),end="")
-    np.save(file, X)
-    print("done")
-
+    if args.features_file is not None:
+        args.features_file = str(args.features_file)
+        if not args.features_file.endswith("npy"):
+            raise ValueError("'features_file' (-ff, --features_file) must have 'npy' extension.")
+        print("\tSaving features to file '{:s}' ... ".format(args.features_file),end="")
+        np.save(file, X)
+        print("done")
 
     #
     if args.number is None :
@@ -116,26 +134,26 @@ def main():
     struct_idx = FPS(n_to_select=n_FPS, progress_bar = True, initialize = 'random').fit(X.T).selected_idx_
     X_fps = X[struct_idx]
 
-    print("\tFPS selected indices: {:d}".format(struct_idx.shape[0]))
+    print("\n\tFPS selected indices: {:d}".format(struct_idx.shape[0]))
     print(f"\tOriginal: {X.shape} ---> FPS: {X_fps.shape}")
 
     # Saving the fps selected structure
     if args.output is not None :
-        print("\tSaving FPS selected structures to file '{:s}' ... ".format(args.output),end="")
+        print("\n\tSaving FPS selected structures to file '{:s}' ... ".format(args.output),end="")
         frames_fps = [frames[i] for i in struct_idx]
         write(args.output, frames_fps, format=args.output_format)
         print("done")
-    else :
-        print("\tOutput file (-o/--output) for the selected structures not specified: they will not be saved to file")
+    # else :
+    #     print("\tOutput file (-o/--output) for the selected structures not specified: they will not be saved to file")
 
     # Saving the fps selected structure
     if args.output_indices :
-        print("\tSaving indices of selected structures to file '{:s}' ... ".format(args.output_indices),end="")
+        print("\n\tSaving indices of selected structures to file '{:s}' ... ".format(args.output_indices),end="")
         indices = np.asarray([ int(i) for i in struct_idx],dtype=int)
         np.savetxt(args.output_indices,indices,fmt='%d')
         print("done")
-    else :
-        print("\tOutput file (-oi/--output_indices) for the indeces of the selected structures not specified: they will not be saved to file")
+    # else :
+    #     print("\tOutput file (-oi/--output_indices) for the indeces of the selected structures not specified: they will not be saved to file")
 
     print("\n\tJob done :)\n")
 
