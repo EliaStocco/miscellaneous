8c04ecc0ddf8f3e2dcceb0869c7e90236a56d0c9
EliaStocco <eliastocco@gmail.com>
2024-01-17 11:38:04 +0100

    maybe I have solved the import problem

--- miscellaneous/elia/nn/dataset/__init__.py
+++ miscellaneous/elia/nn/dataset/__init__.py
@@ -1,4 +1,6 @@
-from miscellaneous.elia.nn.dataset.make_dataset    import make_dataset     as make_dataset
-from miscellaneous.elia.nn.dataset.make_dataset    import make_datapoint   as make_datapoint
-from miscellaneous.elia.nn.dataset.make_dataloader import make_dataloader  as make_dataloader
-from miscellaneous.elia.nn.dataset.make_dataset    import compute_edge_vec as compute_edge_vec
+# from miscellaneous.elia.nn.dataset.make_dataset    import make_dataset     as make_dataset
+# from miscellaneous.elia.nn.dataset.make_dataset    import make_datapoint   as make_datapoint
+# from miscellaneous.elia.nn.dataset.make_dataloader import make_dataloader  as make_dataloader
+# from miscellaneous.elia.nn.dataset.make_dataset    import compute_edge_vec as compute_edge_vec
+from miscellaneous.elia.nn.dataset.make_dataset import *
+from miscellaneous.elia.nn.dataset.make_dataloader import *
--- miscellaneous/elia/nn/network/__init__.py
+++ miscellaneous/elia/nn/network/__init__.py
@@ -1,1 +1,2 @@
 # from .aile3nn import aile3nn as aile3nn
+from miscellaneous.elia.nn.network.aile3nn import aile3nn
