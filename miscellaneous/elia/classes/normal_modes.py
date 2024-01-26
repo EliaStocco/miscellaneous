fc0f0341f8262b49db140229bd2dca44b98bab0e
EliaStocco <eliastocco@gmail.com>
2024-01-26 14:30:03 +0100

    tidied up

--- miscellaneous/elia/classes/normal_modes.py
+++ miscellaneous/elia/classes/normal_modes.py
@@ -11,27 +11,6 @@ import warnings
 # Disable all UserWarnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-def A2B(A,N:xr.DataArray,M:xr.DataArray,E:xr.DataArray):
-    """
-    purpose:
-        convert the A-amplitude [length x mass^{-1/2}] into B-amplitudes [length]
-
-    input :
-        A : A-amplitudes
-        N : normal modes (normalized)
-        M : masses
-        E : eigevectors (of the dynamical matrix)
-
-    output:
-        B : B-amplitudes
-    """
-
-    B = (np.linalg.inv(N) @ MicroState.diag_matrix(M,"-1/2") @ E @ A.T).T
-    if MicroStatePrivate.debug: 
-        print("B shape : ",B.shape)
-
-    return B
-
 def inv(A:xr.DataArray)->xr.DataArray:
     """Calculate the inverse of a 2D ```xarray.DataArray``` using ```np.linalg.inv``` while preserving the xarray structure."""
     if A.ndim != 2:
@@ -123,50 +102,6 @@ class NormalModes(pickleIO):
     def to_dict(self)->dict:
         return nparray2list_in_dict(vars(self))
 
-    # def write(self,file,module="pickle",mode="w"):
-    #     match module:
-    #         case "pickle":
-    #             import pickle
-    #             with open(file, mode) as f:
-    #                 pickle.dump(self, f)
-    #         case "pprint":
-    #             from pprint import pprint
-    #             with open(file, mode) as f:
-    #                 pprint(self, f)
-    #         case "yaml":
-    #             import yaml
-    #             with open(file, mode) as f:
-    #                 yaml.dump(self.to_dict(), f, default_flow_style=False)
-    #         case "json":
-    #             import json
-    #             with open(file, mode) as f:
-    #                 json.dump(self.to_dict(), f)
-    #         case _:
-    #             raise ValueError("saving with module '{:s}' not implemented yet".format(module))
-    
-    # @classmethod
-    # def read(cls,file,module="pickle",mode="r"):
-    #     match module:
-    #         case "pickle":
-    #             import pickle
-    #             with open(file,'rb') as f:
-    #                 loaded_data = pickle.load(f)
-    #         case "yaml":
-    #             import yaml
-    #             with open(file,mode) as f:
-    #                 loaded_data = yaml.load(f)
-    #         case "json":
-    #             import json
-    #             with open(file,mode) as f:
-    #                 loaded_data = json.load(f)
-    #     return cls(loaded_data)
-
-    # def to_pickle(self,file):
-    #     # Open the file in binary write mode ('wb')
-    #     with open(file, 'wb') as file:
-    #         # Use pickle.dump() to serialize and save the object to the file
-    #         pickle.dump(self, file)
-
     @classmethod
     def load(cls,folder=None):    
 
@@ -242,28 +177,6 @@ class NormalModes(pickleIO):
 
     def set_eigval(self,eigval):
         self.eigval[:] = xr.DataArray(eigval, dims=('mode'))
-
-
-    # def set_eigvals(self,band,mode="phonopy"):
-    #     if mode == "phonopy":
-    #         N = self.Nmodes
-    #         eigval = np.full(N,np.nan)
-    #         for n in range(N):
-    #             eigval[n] = band[n]["frequency"]
-    #         self.eigval = np.square(eigval)
-    #     else:
-    #         raise ValueError("not implemented yet")
-    #     pass
-
-    # @property
-    # def freq(self):
-    #     return np.sqrt(np.abs(self.eigval.real)) * np.sign(self.eigval.real)
-        
-    # def diagonalize(self,**argv):
-    #     eigval, eigvecs, = np.linalg.eigh(self.dynmat,**argv)
-    #     frequencies = np.sqrt(np.abs(eigval.real)) * np.sign(eigval.real)
-    #     return frequencies, eigval, eigvecs
-        
     
     def eigvec2modes(self):
         self.non_ortho_mode = self.eigvec.copy()
@@ -276,15 +189,6 @@ class NormalModes(pickleIO):
         # self.old_mode = self.mode.copy()
         # self.mode = self.non_ortho_mode / norm_by(self.non_ortho_mode,"dof")
         # pass
-
-    # def eigvec2proj(self):
-    #     self.proj = self.eigvec.T @ NormalModes.diag_matrix(self.masses,"1/2")
-
-    # def project_displacement(self,displ):
-    #     return self.proj @ displ
-
-    # def project_velocities(self,vel):
-    #     return NormalModes.diag_matrix(self.eigval,"-1/2") @ self.proj @ vel
     
     def build_supercell_displacement(self,size,q):
 
@@ -318,68 +222,7 @@ class NormalModes(pickleIO):
         # supercell.eigvec2proj()
 
         return supercell
-    
-    # def build_supercell_normal_modes(self,size):
-
-    #     from itertools import product
-    #     import cmath
-
-    #     values = [None]*len(size)
-    #     for n,a in enumerate(size):
-    #         values[n] = np.arange(a)
-    #     r_point = list(product(*values))
-    #     k_point = r_point.copy()
-
-    #     size = np.asarray(size)
-    #     N = size.prod()
-    #     supercell = NormalModes(self.Nmodes*N,self.Ndof*N)
-    #     supercell.masses[:] = np.asarray(list(self.masses)*N)
-    #     supercell.eigvec.fill(np.nan)
-    #     for i,r in enumerate(r_point):
-    #         r = np.asarray(r) 
-    #         for j,k in enumerate(k_point):
-    #             kr = np.asarray(k) / size @ r
-    #             phase = np.exp(1.j * 2 * np.pi * kr )
-    #             # phi = int(cmath.phase(phase)*180/np.pi)
-    #             # ic(k,r,phi)
-    #             supercell.eigvec[i*self.Ndof:(i+1)*self.Ndof,j*self.Nmodes:(j+1)*self.Nmodes] = \
-    #                 ( self.eigvec * phase).real
-                
-    #     if np.isnan(supercell.eigvec).sum() != 0:
-    #         raise ValueError("error")
         
-    #     supercell.eigvec /= np.linalg.norm(supercell.eigvec,axis=0)
-        
-    #     supercell.eigvec2modes()
-    #     supercell.eigvec2proj()
-
-    #     return supercell
-    
-    # def remove_dof(self,dof):
-    #     if not hasattr(dof,"__len__"):
-    #         return self.remove_dof([dof])
-        
-    #     out = copy(self)
-
-    #     ii = [x for x in np.arange(self.Ndof) if x not in dof]
-
-    #     # out.ortho_modes = empty.copy()
-    #     out.eigvec = self.eigvec[:,ii]
-    #     out.dynmat = np.nan
-    #     # out.mode = empty.copy()
-    #     # out.proj = empty.copy()
-    #     out.eigval = self.eigval[ii]
-
-    #     out.Nmodes = out.eigvec.shape[1]
-
-    #     out.eigvec2modes()
-    #     # out.eigvec2proj()
-        
-    #     return out
-
-    def A2B(self,A:xr.DataArray)->xr.DataArray:
-        return A2B(A,self.mode,self.masses,self.eigvec)
-    
     def project(self,trajectory,warning="**Warning**"):       
 
         #-------------------#
@@ -600,182 +443,3 @@ class NormalModes(pickleIO):
         # self.mode = save
 
         return out
-
-    # def project(self, trajectory):
-
-    #     import unyt
-    #     # Define the Hartree unit
-    #     unyt.define_unit('hartree', 4.359744650e-18 * unyt.joule)
-
-    #     # Define the Bohr unit
-    #     unyt.define_unit('bohr', 5.291772108e-11 * unyt.meter)
-
-    #     # -------------------#
-    #     use_unyt = True
-
-    #     # -------------------#
-    #     # Create a UnitRegistry with atomic units
-    #     unit_registry = unyt.UnitRegistry()
-
-    #     # Define atomic units based on fundamental constants
-    #     unit = {
-    #         "energy": unyt.hartree if use_unyt else 1,
-    #         "length": unyt.bohr if use_unyt else 1,
-    #         "mass": unyt.me if use_unyt else 1,
-    #         "action": unyt.hbar if use_unyt else 1,
-    #         "dimensionless": unyt.dimensionless if use_unyt else 1
-    #     }
-    #     unit["time"] = unit["action"] / unit["energy"]
-    #     unit["velocity"] = unit["length"] / unit["time"]
-
-    #     # -------------------#
-    #     # reference position
-    #     ref = trajectory[0] if self.reference is None else self.reference
-
-    #     # -------------------#
-    #     # positions -> displacements
-    #     q = trajectory.positions - ref.positions
-    #     q = q.reshape(len(q), -1)
-    #     q *= unit["length"]
-
-    #     # -------------------#
-    #     # velocities
-    #     try:
-    #         v = trajectory.call(lambda e: e.arrays["velocities"])
-    #         v = v.reshape(len(v), -1)
-    #     except:
-    #         warn("velocities not found, setting them to zero.")
-    #         v = np.zeros(q.shape)
-
-    #     v *= unit["velocity"]
-
-    #     # -------------------#
-    #     # building xarrays
-    #     q = xr.DataArray(q, dims=('time', 'dof'))
-    #     v = xr.DataArray(v, dims=('time', 'dof'))
-
-    #     # -------------------#
-    #     # Normal Modes should be real
-    #     save = self.mode.copy()
-    #     if np.any(self.mode.imag != 0.0):
-    #         warn("'mode' matrix should be real --> discarding its imaginary part.")
-    #     # do it anyway
-    #     self.mode = self.mode.real
-
-    #     # -------------------#
-    #     # create projection operator
-    #     proj = self.mode.T * unit["dimensionless"]
-
-    #     # -------------------#
-    #     # simple test
-    #     if use_unyt:
-    #         if not q.units.equal_dimensions(unit_registry.length):
-    #             raise ValueError("displacements have the wrong unit")
-    #         if not v.units.equal_dimensions(unit_registry.length / unit_registry.time):
-    #             raise ValueError("velocities have the wrong unit")
-    #         if not proj.units.equal_dimensions(unit_registry.dimensionless):
-    #             raise ValueError("projection operator has the wrong unit")
-
-    #     # -------------------#
-    #     # project positions and velocities
-    #     qn = proj.dot(q.magnitude, dim="dof")
-    #     vn = proj.dot(v.magnitude, dim="dof")
-
-    #     # -------------------#
-    #     # compute
-    #     m = self.mode.T.dot(self.masses, dim="dof").dot(self.mode, dim="dof")
-    #     w2 = xr.DataArray(self.eigval, dims=('mode'))
-
-    #     # A2 = qn*qn.conjugate() + vn*vn.conjugate()
-    #     # energy = 0.5 * m * (w2 * A2)
-    #     energy = 0.5 * m * (vn * vn.conjugate() + w2 * qn * qn.conjugate())
-    #     A2 = energy / (0.5 * m * w2)
-    #     amplitude = np.sqrt(A2)
-    #     equipartition = energy.shape[energy.dims.index("mode")] * energy / energy.sum("time")
-    #     occupation = energy / np.sqrt(w2)
-    #     phases = np.arctan2(-vn, qn)
-
-    #     out = {
-    #         "energy": energy,
-    #         "amplitude": amplitude,
-    #         "equipartition": equipartition,
-    #         "occupation": occupation,
-    #         "phases": phases
-    #     }
-
-    #     self.mode = save
-
-    #     return out
-
-        
-       
-        # proj_displ = MicroState.project_displacement(q.T,self.proj).T
-        # if not null_vel :
-        #     proj_vel   = MicroState.project_velocities  (v.T,   self.proj, self.eigvals).T
-        # else :
-        #     proj_vel = np.zeros(proj_displ.shape)
-
-        # if skip :
-        #     proj_vel   = proj_vel  [:,Ndof:]
-        #     proj_displ = proj_displ[:,Ndof:]
-        #     w2 = self.eigvals[Ndof:]
-        # else :
-        #     w2 = self.eigvals
-        
-        # A2 = ( np.square(proj_displ) + np.square(proj_vel) )
-        # energy = ( w2 * A2 / 2.0 ) # w^2 A^2 / 2
-        # #energy [ energy == np.inf ] = np.nan
-        # normalized_energy = ( ( self.Nmodes - Ndof ) * energy.T / energy.sum(axis=1).T ).T
-        # Aamplitudes = np.sqrt(A2)
-
-        # # print(norm(proj_displ-c))
-        # # print(norm(proj_vel-s))
-        
-        # # Vs = MicroState.potential_energy_per_mode(proj_displ,self.eigvals)
-        # # Ks = MicroState.kinetic_energy_per_mode  (proj_vel,  self.eigvals)
-        # # Es = Vs + Ks        
-        # # print(norm(energy-Es.T))
-
-        # # self.energy = self.occupations = self.phases = self.Aamplitudes = self.Bamplitudes = None 
-    
-        # # energy = Es.T
-        # occupations = energy / np.sqrt( w2 ) # - 0.5 # hbar = 1 in a.u.
-        # # A  = np.sqrt( 2 * Es.T / self.eigvals  )
-        # # print(norm(A-Aamplitudes))
-        # if skip :
-        #     tmp = np.zeros((Aamplitudes.shape[0],self.Nmodes))
-        #     tmp[:,Ndof:] = Aamplitudes
-        #     Bamplitudes = self.A2B(A=tmp)
-        #     Bamplitudes = Bamplitudes[:,Ndof:]
-        # else :
-        #     Bamplitudes = self.A2B(A=Aamplitudes)
-        
-        # if hasattr(self,"properties") and "time" in self.properties:
-        #     time = convert(self.properties["time"],"time",_from=self.units["time"],_to="atomic_unit")
-        # else :
-        #     time = np.zeros(len(Bamplitudes))
-        # phases = np.arctan2(-proj_vel,proj_displ) - np.outer(np.sqrt( w2 ) , time).T
-        # # phases = np.unwrap(phases,discont=0.0,period=2*np.pi)
-
-        # out = {"energy": energy,\
-        #        "norm-energy": normalized_energy,\
-        #        "occupations": occupations,\
-        #        "phases": phases,\
-        #        "A-amplitudes": Aamplitudes,\
-        #        "B-amplitudes": Bamplitudes}
-        
-        # if inplace:
-        #     self.energy = energy
-        #     self.occupations = occupations
-        #     self.phases = phases
-        #     self.Aamplitudes = Aamplitudes
-        #     self.Bamplitudes = Bamplitudes
-        #     self.normalized_energy = normalized_energy
-
-        # if MicroStatePrivate.debug :
-        #     test = self.project_on_cartesian_coordinates(Aamplitudes,phases,inplace=False)
-        #     #print(norm(test["positions"] - self.positions))
-        #     print(norm(test["displacements"] - q))
-        #     print(norm(test["velocities"] -v))            
-
-        # return out
