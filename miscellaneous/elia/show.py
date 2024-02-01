c2d98f9654b9e729ae5bc3ceaab943a49877f5e6
EliaStocco <eliastocco@gmail.com>
2024-02-01 14:36:22 +0100

    added 'print_df'

--- miscellaneous/elia/show.py
+++ miscellaneous/elia/show.py
@@ -1,4 +1,16 @@
+import pandas as pd
 
 def show_dict(obj:dict,string:str=""):
     for k in obj.keys():
         print("{:s}{:30s}:".format(string,k),obj[k])
+
+def print_df(df:pd.DataFrame)->str:
+    column_names = ''.join(['{:>12}'.format(col) for col in df.columns])
+    print('\n\t' + "-"*len(column_names))
+    print('\t' + column_names)
+    print('\t' + "-"*len(column_names))
+    # Iterate over rows and print with the specified format
+    for index, row in df.iterrows():
+        formatted_row = ''.join(['{:>12.1e}'.format(value) for value in row])
+        print('\t' + formatted_row)
+    print('\t' + "-"*len(column_names))
