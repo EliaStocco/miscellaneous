c1292255b18cac78547705981f28f0ef4ce9472f
EliaStocco <eliastocco@gmail.com>
2024-02-01 14:38:49 +0100

    cleaned file

--- miscellaneous/elia/classes/__init__.py
+++
@@ -1,5 +0,0 @@
-# from miscellaneous.elia.classes.trajectory import trajectory
-# from miscellaneous.elia.classes.properties import properties
-# from miscellaneous.elia.classes.vectorize import easyvectorize
-# from miscellaneous.elia.classes.bec import bec
-# from miscellaneous.elia.classes.normal_modes import NormalModes
